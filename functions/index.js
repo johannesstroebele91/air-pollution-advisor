8785a1804a81bb9ea4db84c9d8e9c5aa2eeb46d6
Johannes Ströbele <stroebele.j@gmail.com>
2021-12-07 21:32:49 +0100

    Add firebase function that is connected to firestore

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,7 @@
 const functions = require("firebase-functions");
 const express = require("express");
+const { getFirestore } = require("firebase-admin/firestore");
+const https = require("https");
 
 const admin = require("firebase-admin");
 admin.initializeApp();
@@ -16,6 +18,34 @@ app.post("/", (req, res, next) => {
     return;
   }
   res.status(200).send({ number: req.body.number + 5 });
+});
+
+const db = getFirestore();
+
+// Adapt the options based on API
+const options = {
+  host: "api.somesite.io",
+  path: "/some-path/",
+  headers: {
+    Authorization: "Basic admin:test",
+  },
+};
+
+db.collection("alarms").onSnapshot((querySnapshot) => {
+  querySnapshot.docChanges().forEach((change) => {
+    if (change.type === "added") {
+      https
+        .get(options, (response) => {
+          response.on("data", function (chunk) {
+            console.log(JSON.parse(chunk));
+            db.collection("/alarms")
+              .doc(change.doc.id)
+              .update({ alarm: JSON.parse(chunk).alarm });
+          });
+        })
+        .end();
+    }
+  });
 });
 
 exports.api = functions.https.onRequest(app);
